b11e2bd86d1808944d96d67967da4c81f2b9223b
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2024-08-09 13:39:27 +0200

    Swagger

--- backend/application.py
+++ backend/application.py
@@ -4,10 +4,21 @@ from users.user import User
 from flask_login import LoginManager, login_required
 from users.user_repository import UserRepository
 from routes.authorisation_blueprint import authorisation_blueprint
+from flask_swagger_ui import get_swaggerui_blueprint
 # python -m flask --app .\application.py run
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={ 'app_name': "User-Behavior-Driven-Interface Backend API" }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
 Database.initialise()
 login = LoginManager(app)
 
