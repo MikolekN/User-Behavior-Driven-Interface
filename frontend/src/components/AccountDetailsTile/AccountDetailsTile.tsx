26ae75bcfe2b714183feb557a806e6d281c37f04
dlydka <lydkadm@gmail.com>
2024-08-26 12:59:34 +0200

    Added account details tile

---
+++ frontend/src/components/AccountDetailsTile/AccountDetailsTile.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Tile from '../Tile/Tile';
+import './AccountDetailsTile.css';
+import Button from '../utils/Button';
+
+// pozniej przerobimy to na odpytanie api, na ten moment dane są tylko na potrzeby frontend'owe
+const data = {
+    accountName: "Konto dla Młodych",
+    accountNumber: "06 1020 5040 1234 6001 0231 6177",
+    availableFunds: 123.56,
+    balance: 99.12,
+    blockades: 44.00,
+    currency: "PLN"
+};
+
+const availableFunds = data.availableFunds.toFixed(2);
+const balance = data.balance.toFixed(2);
+const blockades = data.blockades.toFixed(2);
+//
+
+const AccountDetailsTile = () => {
+  return (
+    <Tile title={data.accountName}>
+        <div>
+            <p>{data.accountNumber}</p>
+        </div>
+        <div className="flex justify-end space-x-4 ml-10">
+            <div className="p-4">
+                <p>Balance: {balance} {data.currency}</p>
+                <p>Blockades: {blockades} {data.currency}</p>
+            </div>
+            <div className="grid p-4">
+                <p className="border-l border-gray-500 pl-8">Avaiable Funds: <br/> {availableFunds} {data.currency}</p>
+                <div className="justify-self-end pt-4 pl-4">
+                    <Button>Transfer</Button>
+                </div>
+            </div>
+        </div>
+    </Tile>
+  )
+}
+
+export default AccountDetailsTile
---
+++ frontend/src/components/utils/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ReactElement, ReactNode } from 'react';
+
+interface ButtonProps {
+    children: ReactNode | ReactElement;
+    onClick?: () => void;
+    className?: string
+}
+
+const Button = ({children, onClick, className}: ButtonProps) => {
+  return (
+    <button onClick={onClick} className={`${className} py-3 px-4 bg-blue-600 hover:bg-blue-700 rounded-lg text-white font-semibold text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50`}>
+        {children}
+    </button>
+  )
+}
+
+export default Button
