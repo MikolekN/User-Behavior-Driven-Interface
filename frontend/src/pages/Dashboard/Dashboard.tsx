51e9d7a7489d457e97ee3b855e805fc024563930
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2024-09-19 15:46:09 +0200

    Fixed dashboard

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -12,25 +12,33 @@ const Dashboard = () => {
     if (!user) return <Navigate to="/login" />;
 
     return (
-        <Tile title={user.accountName} className="dashboard-tile">
-            <div className="account-number-container" aria-label="Account number">
-                <img src={icon} alt="Credit Card Icon" className="credit-card-icon" />
-                <p className="account-number">{user.accountNumber}</p>
-            </div>
-            <div className="info-container">
-                <div className="balance-info-section">
-                    <p>Balance: {user.balance} {user.currency}</p>
-                    <p>Blockades: {user.blockades} {user.currency}</p>
-                    <div className='separator' />
-                    <p className='available-funds'>Available Funds: {user.availableFunds} {user.currency}</p>
+        <div className="flex items-center justify-center">
+            <Tile title={user.accountName} className="min-w-fit w-1/3 flex flex-col p-2.5">
+                <div className="flex justify-between items-stretch p-5">
+                    <div className="flex-grow flex flex-col items-center min-w-fit mr-5">
+                        <p>Balance: {user.balance} {user.currency}</p>
+                        <p>Blockades: {user.blockades} {user.currency}</p>
+                        <div>
+                            <div className='border-t border-gray-300 my-3.5 w-full' />
+                            <p className='font-semibold text-gray-800'>Available Funds: {user.availableFunds} {user.currency}</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center w-fit">
+                        <div className="flex items-center mb-2.5">
+                            <img src={icon} alt="" className="w-5 mr-1.5" />
+                            <p>{user.accountNumber}</p>
+                        </div>
+                        <div className="flex-grow"></div>
+                        <div className="flex justify-center">
+                            <Link to="/transfer">
+                                <Button>Transfer</Button>
+                            </Link>
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className="transfer-button" aria-label="Transfer Funds">
-                    <Link to="/transfer">
-                        <Button>Transfer</Button>
-                    </Link>
-                </div>
-            </div>
-        </Tile>
+            </Tile>
+        </div>
     );
 };
 
