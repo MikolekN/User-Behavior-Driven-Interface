dd49e8b61288b816acf7b4617609c9af29eb933c
dlydka <lydkadm@gmail.com>
2024-10-09 18:58:36 +0200

    transactions history hotfix

--- frontend/src/components/TransactionsHistory/TransactionsHistory.tsx
+++ frontend/src/components/TransactionsHistory/TransactionsHistory.tsx
@@ -31,7 +31,7 @@ const TransactionsHistory = () => {
         const fetchTransactions = async () => {
             try {
                 const response = await fetch('http://127.0.0.1:5000/api/transfers', {
-                    method: "PUT",
+                    method: "GET",
                     headers: {
                     "Content-Type": "application/json" 
                     },
