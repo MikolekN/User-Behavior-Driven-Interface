c646bff4a9adafd4dc87b137300fc2ffa9b3f6f0
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2024-10-10 13:44:02 +0200

    Added more eslint rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,15 +4,27 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'airbnb-typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'semi': [2, 'always'],
+    'react/react-in-jsx-scope': 'off',
   },
-}
+};
