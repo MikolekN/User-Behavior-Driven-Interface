2d6caad3556fe245ecd2e4ccddf05fdd26b11ed7
dlydka <lydkadm@gmail.com>
2024-10-16 17:19:50 +0200

    added items to constants.ts

--- frontend/src/services/constants.ts
+++ frontend/src/services/constants.ts
@@ -1,5 +1,6 @@
 export const API_URL = 'http://127.0.0.1:5000/api';
 export const DAY_LENGTH_IN_MILISECONDS = 86400000;
+
 export const AVAILABLE_LOAN_LENGTH = [
     { loanLength: 6 },
     { loanLength: 12 },
@@ -10,3 +11,10 @@ export const AVAILABLE_LOAN_LENGTH = [
 export const MIN_LOAN_AMOUNT = 1000;
 export const MAX_LOAN_AMOUNT = 100000;
 export const LOAN_AMOUNT_STEP = 1000;
+
+export const CHART_HEIGHT: number = 400;
+export const LEGEND_HEIGHT: number = 30;
+export const COLORS = {
+    RED: '#ee0000',
+    GREEN: '#00d800'
+};
