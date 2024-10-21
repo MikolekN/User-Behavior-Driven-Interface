32b0432d16b9b6dd9418709ccb514edb1261e147
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2024-10-21 21:17:29 +0200

    Docker changes (#24)
    * change in debug mode
    
    * Update vite for docker

--- backend/application.py
+++ backend/application.py
@@ -50,4 +50,4 @@ app.register_blueprint(user_blueprint)
 app.register_blueprint(cyclic_payment_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=app.config['DEBUG_MODE'], host="0.0.0.0")
