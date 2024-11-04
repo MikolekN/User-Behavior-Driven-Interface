0a3fb30281f23d7380409adc5347f90763303905
dlydka <lydkadm@gmail.com>
2024-11-04 20:36:00 +0100

    add tests for cyclic payments endpoints

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+from backend.cyclic_payments.cyclic_payment import CyclicPayment
 from backend.users.user import User
 import bcrypt
 import pytest
 from ..application import create_app
-from .constants import TEST_EMAIL, TEST_PASSWORD, TEST_ID
+from backend.tests.constants import TEST_EMAIL, TEST_ID, TEST_PASSWORD
+from backend.tests.cyclic_payment.constants import TEST_AMOUNT, TEST_CYCLIC_PAYMENT_INTERVAL, TEST_CYCLIC_PAYMENT_NAME, TEST_CYCLIC_PAYMENT_START_DATE, TEST_CYCLIC_PAYMENT_TRANSFER_TITLE, TEST_ISSUER_ACCOUNT_NUMBER, TEST_RECIPIENT_ACCOUNT_NUMBER
 
 @pytest.fixture
 def app():
@@ -35,4 +38,48 @@ def test_user():
         currency="PLN",
         user_icon=None,
         role="USER"
+    )
+
+@pytest.fixture
+def test_recipient_user():
+    hashed_password = bcrypt.hashpw(TEST_PASSWORD.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    yield User(
+        _id=TEST_ID,
+        login="test_recipient_user",
+        email=TEST_EMAIL,
+        password=hashed_password,
+        account_name="Test Account",
+        account_number=TEST_RECIPIENT_ACCOUNT_NUMBER,
+        blockades=0.0,
+        balance=2000.0,
+        currency="PLN",
+        user_icon=None,
+        role="USER"
+    )
+
+@pytest.fixture
+def test_issuer_user():
+    hashed_password = bcrypt.hashpw(TEST_PASSWORD.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    yield User(
+        _id=TEST_ID,
+        login="test_issuer_user",
+        email=TEST_EMAIL,
+        password=hashed_password,
+        account_name="Test Account",
+        account_number=TEST_ISSUER_ACCOUNT_NUMBER,
+        blockades=0.0,
+        balance=2000.0,
+        currency="PLN",
+        user_icon=None,
+        role="USER"
+    )
+
+@pytest.fixture
+def test_cyclic_payment():
+    yield CyclicPayment(
+        issuer_id=TEST_ID, recipient_id=TEST_ID, 
+        recipient_account_number=TEST_RECIPIENT_ACCOUNT_NUMBER, recipient_name="test name",
+        cyclic_payment_name=TEST_CYCLIC_PAYMENT_NAME, transfer_title=TEST_CYCLIC_PAYMENT_TRANSFER_TITLE, 
+        amount=float(TEST_AMOUNT), start_date=datetime.fromisoformat(TEST_CYCLIC_PAYMENT_START_DATE), 
+        interval=TEST_CYCLIC_PAYMENT_INTERVAL
     )
---
+++ backend/tests/cyclic_payment/test_delete_cyclic_payment.py
@@ -0,0 +1,45 @@
+from unittest.mock import MagicMock, patch
+from backend.tests.cyclic_payment.constants import TEST_CYCLIC_PAYMENT_ID, TEST_CYCLIC_PAYMENT_INVALID_ID
+
+def test_delete_cyclic_payment_unauthorized(client):
+    response = client.delete(f'/api/cyclic-payment/{TEST_CYCLIC_PAYMENT_ID}')
+    assert response.status_code == 401
+
+@patch('backend.cyclic_payments.cyclic_payment_repository.CyclicPaymentRepository.find_by_id')
+def test_delete_cyclic_payment_invalid_object_id(mock_find_by_id, client, test_user, test_cyclic_payment):
+    with patch('flask_login.utils._get_user', return_value=test_user):
+        mock_find_by_id.return_value = test_cyclic_payment
+
+        response = client.delete(f'/api/cyclic-payment/{TEST_CYCLIC_PAYMENT_INVALID_ID}')
+
+        assert response.status_code == 400
+        json_data = response.get_json()
+        assert 'message' in json_data
+        assert json_data['message'] == "Invalid Object ID"
+
+@patch('backend.cyclic_payments.cyclic_payment_repository.CyclicPaymentRepository.find_by_id')
+def test_delete_cyclic_payment_not_exist(mock_find_by_id, client, test_user):
+    with patch('flask_login.utils._get_user', return_value=test_user):
+        mock_find_by_id.return_value = None
+
+        response = client.delete(f'/api/cyclic-payment/{TEST_CYCLIC_PAYMENT_ID}')
+
+        assert response.status_code == 404
+        json_data = response.get_json()
+        assert 'message' in json_data
+        assert json_data['message'] == "Cyclic Payment with given ID does not exist"
+
+@patch('backend.users.user_repository.UserRepository.find_by_id')
+@patch('backend.cyclic_payments.cyclic_payment_repository.CyclicPaymentRepository.find_by_id')
+def test_delete_cyclic_payment_success(mock_find_cyclic_payment_by_id, mock_find_user_by_id, client, test_user, test_cyclic_payment):
+    with patch('flask_login.utils._get_user', return_value=test_user):
+        mock_find_cyclic_payment_by_id.return_value = test_cyclic_payment
+        mock_find_user_by_id.return_value = test_user
+
+        response = client.delete(f'/api/cyclic-payment/{TEST_CYCLIC_PAYMENT_ID}')
+
+        assert response.status_code == 200
+        json_data = response.get_json()
+        assert 'message' in json_data
+        assert json_data['message'] == "Cyclic Payment deleted successfully"
+
