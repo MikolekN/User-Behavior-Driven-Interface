d27cc189b638551bff00fa41083a2c9aba1f66fa
dlydka <lydkadm@gmail.com>
2024-11-16 11:03:04 +0100

    custom alerts

--- frontend/src/components/Alerts/InfoAlert.tsx
+++ frontend/src/components/Alerts/InfoAlert.tsx
@@ -10,7 +10,6 @@ const InfoAlert = ({alertTitle, alertMessage}: AlertProps) => {
         <div id="info-alert" className="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
             <div className="flex items-center">
                 <HiInformationCircle />
-                <span className="sr-only">Info</span>
                 <h3 className="text-lg font-medium ml-2">{alertTitle}</h3>
             </div>
             <div className="mt-2 mb-4 text-sm">
