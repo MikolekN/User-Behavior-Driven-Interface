6a7810d41e58b549d2a7a8dda4becac7ba573525
dlydka <lydkadm@gmail.com>
2024-11-20 18:39:09 +0100

    small fix

--- frontend/src/components/CyclicPaymentForm/CyclicPaymentForm.tsx
+++ frontend/src/components/CyclicPaymentForm/CyclicPaymentForm.tsx
@@ -84,10 +84,10 @@ const CyclicPaymentsForm = () => {
         }
     }, [cyclicPayment, setCyclicPaymentFormEditValues, setCyclicPaymentFormDefaultValues]);
 
-    function toLocalISOString(date: Date) {
+    const toLocalISOString = (date: Date) => {
         const localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
         return localDate.toISOString();
-      }
+    };
 
     const onSubmit = handleSubmit(async (data: CyclicPaymentFormData) => {
         if (cyclicPayment === null) {
