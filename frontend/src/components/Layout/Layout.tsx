f4743ede89cbbc7ed2a8311e3a4ca5b73b72df86
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2024-12-02 10:22:21 +0100

    Basic responsiveness and dark mode

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -15,7 +15,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
                     <MainMenu />
                 </MegaMenu>
             </header>
-            <main id='layout-content-area' className='grid flex-grow overflow-y-auto'>
+            <main id='layout-content-area' className='grid flex-grow overflow-y-auto bg-gray-100 dark:bg-gray-800'>
                 {children}
             </main>
             <footer id="layout-footer" className="dark:bg-gray-800 dark:text-gray-400 text-center py-1 text-sm">
--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -4,6 +4,7 @@ import { UserContext } from '../../context/UserContext';
 import Tile from '../../components/Tile/Tile';
 import Button from '../../components/utils/Button';
 import icon from '../../assets/images/credit-card.png';
+import iconDark from '../../assets/images/credit-card-dark.png';
 
 const Dashboard = () => {
     const { user } = useContext(UserContext);
@@ -13,18 +14,19 @@ const Dashboard = () => {
     return (
         <div className="flex items-center justify-center">
             <Tile title={user.accountName} id="dashboard" className="min-w-fit w-1/3 flex flex-col p-2.5">
-                <div className="flex justify-between items-stretch p-5">
+                <div className="text-black dark:text-gray-400 md:flex md:justify-between items-stretch p-5">
                     <div className="flex-grow flex flex-col items-center min-w-fit mr-5">
                         <p>Balance: {user.balance} {user.currency}</p>
                         <p>Blockades: {user.blockades} {user.currency}</p>
                         <div>
                             <div className='border-t border-gray-300 my-3.5 w-full' />
-                            <p className='font-semibold text-gray-800'>Available Funds: {user.availableFunds} {user.currency}</p>
+                            <p className='font-semibold text-black dark:text-gray-400'>Available Funds: {user.availableFunds} {user.currency}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center w-fit">
+                    <div className="pt-8 md:pt-0 flex flex-col items-center w-fit">
                         <div className="flex items-center mb-2.5">
-                            <img src={icon} alt="" className="w-5 mr-1.5" />
+                            <img src={icon} alt="" className="dark:hidden w-5 mr-1.5" />
+                            <img src={iconDark} alt="" className="hidden dark:block w-5 mr-1.5" />
                             <p>{user.accountNumber}</p>
                         </div>
                         <div className="flex-grow"></div>
