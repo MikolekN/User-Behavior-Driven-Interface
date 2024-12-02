88442be0b4ead026d1ebfec8a8953a8f08a46e53
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2024-12-02 14:40:18 +0100

    Dashboard (#37)
    * Basic responsiveness and dark mode
    
    * Correct mobile view
    
    * Final changes

--- frontend/src/components/Layout/Logo.tsx
+++ frontend/src/components/Layout/Logo.tsx
@@ -6,7 +6,7 @@ export const Logo = () => {
     return (
         <Navbar.Brand as={Link} to="/" className="order-2 md:order-1">
             <img src={logo} className="h-10" alt="Logo" />
-            <span className="ml-2 self-center whitespace-nowrap text-lg font-medium dark:text-white hidden md:block text-black">Bank</span>
+            <span className="ml-2 self-center whitespace-nowrap text-lg font-medium dark:text-gray-400 hidden md:block text-black">Bank</span>
         </Navbar.Brand>
     );
 };
--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -4,6 +4,7 @@ import { UserContext } from '../../context/UserContext';
 import Tile from '../../components/Tile/Tile';
 import Button from '../../components/utils/Button';
 import icon from '../../assets/images/credit-card.png';
+import iconDark from '../../assets/images/credit-card-dark.png';
 
 const Dashboard = () => {
     const { user } = useContext(UserContext);
@@ -13,26 +14,29 @@ const Dashboard = () => {
     return (
         <div className="flex items-center justify-center">
             <Tile title={user.accountName} id="dashboard" className="min-w-fit w-1/3 flex flex-col p-2.5">
-                <div className="flex justify-between items-stretch p-5">
-                    <div className="flex-grow flex flex-col items-center min-w-fit mr-5">
+                <div className="text-black dark:text-gray-400 flex flex-col md:flex-row p-5 justify-center items-center">
+                    <div className="order-2 md:order-1 flex flex-col items-center min-w-fit mb-4">
                         <p>Balance: {user.balance} {user.currency}</p>
                         <p>Blockades: {user.blockades} {user.currency}</p>
                         <div>
                             <div className='border-t border-gray-300 my-3.5 w-full' />
-                            <p className='font-semibold text-gray-800'>Available Funds: {user.availableFunds} {user.currency}</p>
+                            <p className='font-semibold text-black dark:text-gray-400 '>Available Funds: {user.availableFunds} {user.currency}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col items-center w-fit">
-                        <div className="flex items-center mb-2.5">
-                            <img src={icon} alt="" className="w-5 mr-1.5" />
+                    <div className="order-1 mb-4 md:mb-0 md:ml-4 flex md:flex-col items-center justify-center">
+                        <div className='flex items-center'>
+                            <img src={icon} alt="" className="dark:hidden w-5 h-5 mr-1.5" />
+                            <img src={iconDark} alt="" className="hidden dark:block w-5 h-5 mr-1.5" />
                             <p>{user.accountNumber}</p>
                         </div>
-                        <div className="flex-grow"></div>
-                        <div className="flex justify-center">
-                            <Link to="/transfer">
-                                <Button>Transfer</Button>
-                            </Link>
-                        </div>
+                        <Link to="/transfer" className='hidden md:block mt-2'>
+                            <Button>Transfer</Button>
+                        </Link>
+                    </div>
+                    <div className="order-2 w-fit md:hidden">
+                        <Link to="/transfer">
+                            <Button>Transfer</Button>
+                        </Link>
                     </div>
                 </div>
             </Tile>
