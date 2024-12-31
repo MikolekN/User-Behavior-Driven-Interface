bbee29680c740bf23eadb7add75aea02371b3e0c
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2024-12-31 11:57:36 +0100

    Fix icon tests

--- backend/tests/user_icon/test_get_user_icon.py
+++ backend/tests/user_icon/test_get_user_icon.py
@@ -5,7 +5,7 @@ def test_upload_user_icon_not_logged_in(client):
     response = client.get('/api/user/icon')
     assert response.status_code == 401
 
-@patch('backend.users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon=None))
+@patch('users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon=None))
 def test_get_user_icon_no_icon_set(mock_find_by_id, client, test_user):
     with patch('flask_login.utils._get_user', return_value=test_user):
         response = client.get('/api/user/icon')
@@ -15,8 +15,7 @@ def test_get_user_icon_no_icon_set(mock_find_by_id, client, test_user):
         assert 'message' in json_data
         assert json_data['message'] == "iconNotSetForUser"
 
-# TODO: FIX
-@patch('backend.users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon="/path/to/nonexistent/icon.png"))
+@patch('users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon="/path/to/nonexistent/icon.png"))
 @patch('os.path.exists', return_value=False)
 def test_get_user_icon_file_not_found(mock_exists, mock_find_by_id, client, test_user):
     with patch('flask_login.utils._get_user', return_value=test_user):
@@ -27,8 +26,7 @@ def test_get_user_icon_file_not_found(mock_exists, mock_find_by_id, client, test
         assert 'message' in json_data
         assert json_data['message'] == "iconUserNotFound"
 
-# TODO: FIX
-@patch('backend.users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon="/path/to/existing/icon.png"))
+@patch('users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon="/path/to/existing/icon.png"))
 @patch('os.path.exists', return_value=True)
 @patch('flask.helpers.send_file', side_effect=Exception("File access error"))
 def test_get_user_icon_file_access_error(mock_send_file, mock_exists, mock_find_by_id, client, test_user):
@@ -38,10 +36,9 @@ def test_get_user_icon_file_access_error(mock_send_file, mock_exists, mock_find_
         assert response.status_code == 500
         json_data = response.get_json()
         assert 'message' in json_data
-        assert 'Failed to send the icon:' in json_data['message']
+        assert 'sendIconFailed;' in json_data['message']
 
-# TODO: FIX
-@patch('backend.users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon="tests/user_icon/test_icon.png"))
+@patch('users.user_repository.UserRepository.find_by_id', return_value=MagicMock(user_icon="tests/user_icon/test_icon.png"))
 @patch('os.path.exists', return_value=True)
 @patch('flask.helpers.send_file', return_value=MagicMock(status_code=200))
 def test_get_user_icon_success(mock_send_file, mock_exists, mock_find_by_id, client, test_user):
