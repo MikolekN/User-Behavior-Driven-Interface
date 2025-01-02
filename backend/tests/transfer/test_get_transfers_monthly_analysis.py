2c2f9fa0501aa990717daa376a9fc4d8732e5b0e
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-01-02 11:03:32 +0100

    Fix monthly analysis tests

--- backend/tests/transfer/test_get_transfers_monthly_analysis.py
+++ backend/tests/transfer/test_get_transfers_monthly_analysis.py
@@ -7,7 +7,7 @@ def test_get_transfers_monthly_analysis_unauthorized(client):
     response = client.post('/api/transfers/analysis/monthly')
     assert response.status_code == 401
 
-@patch('backend.transfers.transfer_repository.TransferRepository.find_transfers')
+@patch('transfers.transfer_repository.TransferRepository.find_transfers')
 def test_get_transfers_monthly_analysis_empty_data(mock_find_transfers, client, test_user):
     with patch('flask_login.utils._get_user', return_value=test_user):
         mock_find_transfers.return_value = None
@@ -19,7 +19,7 @@ def test_get_transfers_monthly_analysis_empty_data(mock_find_transfers, client, 
         assert 'message' in json_data
         assert json_data['message'] == "emptyRequestPayload"
 
-@patch('backend.transfers.transfer_repository.TransferRepository.find_transfers')
+@patch('transfers.transfer_repository.TransferRepository.find_transfers')
 def test_get_transfers_monthly_analysis_invalid_data(mock_find_transfers, client, test_user):
     with patch('flask_login.utils._get_user', return_value=test_user):
         mock_find_transfers.return_value = None
@@ -31,7 +31,7 @@ def test_get_transfers_monthly_analysis_invalid_data(mock_find_transfers, client
         assert 'message' in json_data
         assert json_data['message'] == "yearNotProvided"
 
-@patch('backend.transfers.transfer_repository.TransferRepository.find_transfers')
+@patch('transfers.transfer_repository.TransferRepository.find_transfers')
 def test_get_transfers_monthly_analysis_invalid_data_type(mock_find_transfers, client, test_user):
     with patch('flask_login.utils._get_user', return_value=test_user):
         mock_find_transfers.return_value = None
@@ -43,8 +43,7 @@ def test_get_transfers_monthly_analysis_invalid_data_type(mock_find_transfers, c
         assert 'message' in json_data
         assert json_data['message'] == "yearInvalidType"
 
-# TODO: FIX
-@patch('backend.transfers.transfer_repository.TransferRepository.find_transfers')
+@patch('transfers.transfer_repository.TransferRepository.find_transfers')
 def test_get_transfers_monthly_analysis_not_exist(mock_find_transfers, client, test_user):
     with patch('flask_login.utils._get_user', return_value=test_user):
         mock_find_transfers.return_value = None
@@ -54,11 +53,11 @@ def test_get_transfers_monthly_analysis_not_exist(mock_find_transfers, client, t
         assert response.status_code == 404
         json_data = response.get_json()
         assert 'message' in json_data
-        assert json_data['message'] == "Monthly transfers analysis for current user is empty"
+        assert json_data['message'] == "monthlyAnalysisEmpty"
 
 
-@patch('backend.transfers.transfer_repository.TransferRepository.find_transfers')
-@patch('backend.users.user_repository.UserRepository.find_by_id')
+@patch('transfers.transfer_repository.TransferRepository.find_transfers')
+@patch('users.user_repository.UserRepository.find_by_id')
 def test_get_transfers_monthly_analysis_success(mock_find_user_by_id, mock_find_transfers, client, test_issuer_user, test_transfers_for_analysis, test_user):
     with patch('flask_login.utils._get_user', return_value=test_issuer_user):
         mock_find_transfers.return_value = test_transfers_for_analysis
