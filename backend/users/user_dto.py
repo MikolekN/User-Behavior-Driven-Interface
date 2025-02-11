ea851daa92aeac624028e70e13fd3d53eada877b
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-02-11 12:19:29 +0100

    Add active account to user

--- backend/users/user_dto.py
+++ backend/users/user_dto.py
@@ -9,13 +9,15 @@ class UserDto:
     login: str
     email: str
     role: str
+    active_account: str
 
     @classmethod
     def from_user(cls, user: User) -> "UserDto":
         return cls(
             login=user.login or '',
             email=user.email or '',
-            role=user.role or 'USER'
+            role=user.role or 'USER',
+            active_account=str(user.active_account) or ''
         )
 
     def to_dict(self) -> Dict[str, Any]:
@@ -23,4 +25,5 @@ class UserDto:
             "login": self.login,
             "email": self.email,
             "role": self.role,
+            "active_account": self.active_account
         }
