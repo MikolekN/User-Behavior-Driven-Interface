dcb0e12dbe3abd1db19b95598ef64cd826fdc4ca
dlydka <lydkadm@gmail.com>
2025-02-17 14:23:51 +0100

    prepared Dashboard for not active account set

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -4,42 +4,58 @@ import Button from '../../components/utils/Button';
 import icon from '../../assets/images/credit-card.png';
 import iconDark from '../../assets/images/credit-card-dark.png';
 import { useTranslation } from 'react-i18next';
-import { useContext } from 'react';
-import { UserContext } from '../../context/UserContext';
+import { useContext, useEffect } from 'react';
+import { AccountContext } from '../../context/AccountContext';
 
 const Dashboard = () => {
     const { t } = useTranslation();
-    const { user } = useContext(UserContext);
+    const { account, getActiveAccount } = useContext(AccountContext);
+
+    useEffect(() => {
+        const getActiveUserAccount = async () => {
+            await getActiveAccount();
+        }
+
+        getActiveUserAccount();
+    }, []);
 
     return (
         <div className="flex items-center justify-center">
-            <Tile title={user!.accountName} id="dashboard" className="min-w-fit w-1/3 flex flex-col p-2.5">
-                <div className="text-black dark:text-gray-400 flex flex-col md:flex-row p-5 justify-center items-center">
-                    <div className="order-2 md:order-1 flex flex-col items-center min-w-fit mb-4">
-                        <p>{t('dashboard.balance') + ': '}{user!.balance} {user!.currency}</p>
-                        <p>{t('dashboard.blockades') + ': '}{user!.blockades} {user!.currency}</p>
-                        <div>
-                            <div className='border-t border-gray-300 my-3.5 w-full' />
-                            <p className='font-semibold text-black dark:text-gray-400 '>{t('dashboard.availableFunds') + ': '}{user!.availableFunds} {user!.currency}</p>
+            {!account ? ( 
+                <Tile title={t('dashboard.userDontHaveAccount')} id="account-not-set" className="min-w-fit w-1/3 flex flex-col p-2.5">
+                    <Link to={'/create-account/'} className="justify-self-end p-2">
+                        <Button className="dark:bg-slate-900 dark:hover:bg-slate-800">+ {t('accountList.submit')}</Button>
+                    </Link>
+                </Tile>
+            ) : (
+                <Tile title={account!.accountName} id="dashboard" className="min-w-fit w-1/3 flex flex-col p-2.5">
+                    <div className="text-black dark:text-gray-400 flex flex-col md:flex-row p-5 justify-center items-center">
+                        <div className="order-2 md:order-1 flex flex-col items-center min-w-fit mb-4">
+                            <p>{t('dashboard.balance') + ': '}{account!.balance} {account!.currency}</p>
+                            <p>{t('dashboard.blockades') + ': '}{account!.blockades} {account!.currency}</p>
+                            <div>
+                                <div className='border-t border-gray-300 my-3.5 w-full' />
+                                <p className='font-semibold text-black dark:text-gray-400 '>{t('dashboard.availableFunds') + ': '}{account!.availableFunds} {account!.currency}</p>
+                            </div>
+                        </div>
+                        <div className="order-1 mb-4 md:mb-0 md:ml-4 flex md:flex-col items-center justify-center">
+                            <div className='flex items-center'>
+                                <img src={icon} alt="" className="dark:hidden w-5 h-5 mr-1.5" />
+                                <img src={iconDark} alt="" className="hidden dark:block w-5 h-5 mr-1.5" />
+                                <p>{account!.accountNumber}</p>
+                            </div>
+                            <Link to="/transfer" className='hidden md:block mt-2'>
+                                <Button className="dark:bg-slate-900 dark:hover:bg-slate-800">{t('dashboard.transfer')}</Button>
+                            </Link>
+                        </div>
+                        <div className="order-2 w-fit md:hidden">
+                            <Link to="/transfer">
+                                <Button className="dark:bg-slate-900 dark:hover:bg-slate-800">{t('dashboard.transfer')}</Button>
+                            </Link>
                         </div>
                     </div>
-                    <div className="order-1 mb-4 md:mb-0 md:ml-4 flex md:flex-col items-center justify-center">
-                        <div className='flex items-center'>
-                            <img src={icon} alt="" className="dark:hidden w-5 h-5 mr-1.5" />
-                            <img src={iconDark} alt="" className="hidden dark:block w-5 h-5 mr-1.5" />
-                            <p>{user!.accountNumber}</p>
-                        </div>
-                        <Link to="/transfer" className='hidden md:block mt-2'>
-                            <Button className="dark:bg-slate-900 dark:hover:bg-slate-800">{t('dashboard.transfer')}</Button>
-                        </Link>
-                    </div>
-                    <div className="order-2 w-fit md:hidden">
-                        <Link to="/transfer">
-                            <Button className="dark:bg-slate-900 dark:hover:bg-slate-800">{t('dashboard.transfer')}</Button>
-                        </Link>
-                    </div>
-                </div>
-            </Tile>
+                </Tile>
+            )}
         </div>
     );
 };
