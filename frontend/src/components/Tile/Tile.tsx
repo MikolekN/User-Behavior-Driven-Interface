3deb3747929bea7e4df0d2a30ee6ea6bacb1b094
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-02-21 15:05:49 +0100

    Add gap between Tile and header or footer

--- frontend/src/components/Tile/Tile.tsx
+++ frontend/src/components/Tile/Tile.tsx
@@ -16,8 +16,8 @@ const Tile: FC<TileProps> = ({ children, title, id, className = '' }) => {
             <div
                 id={`${id}-tile`}
                 className={`bg-white dark:bg-gray-700 p-2 md:p-6 rounded-xl
-                    shadow-lg hover:shadow-2xl transition-shadow duration-[0.3s] ease
-                    w-full md:w-fit md:max-w-full md:min-w-[50%] md:max-h-full flex flex-col
+                    shadow-md hover:shadow-lg transition-shadow duration-[0.3s] ease
+                    w-full md:w-fit md:max-w-full md:min-w-[50%] md:max-h-[96%] flex flex-col my-4
                     ${className}`}
             >
                 <h2 id={`${id}-tile-title`} className="m-0 md:pb-2 md:border-b-2 border-solid text-xl md:text-3xl text-center font-semibold md:min-h-fit flex-none">{title}</h2>
