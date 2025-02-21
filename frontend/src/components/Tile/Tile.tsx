3bf4767554afda15270393496fb9603f971e44bd
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-02-21 15:32:58 +0100

    Works - but not great

--- frontend/src/components/Tile/Tile.tsx
+++ frontend/src/components/Tile/Tile.tsx
@@ -11,17 +11,18 @@ const Tile: FC<TileProps> = ({ children, title, id, className = '' }) => {
     return (
         <div
             id={`${id}-wrapper`}
-            className='flex flex-col flex-grow justify-center items-center min-h-fit h-full w-[90%] max-w-[90%] md:max-w-[70%] md:max-h-full'
+            className='flex flex-col flex-grow justify-center items-center h-full w-[90%] max-w-[90%] md:max-w-[70%] md:h-full'
         >
             <div
                 id={`${id}-tile`}
                 className={`bg-white dark:bg-gray-700 p-2 md:p-6 rounded-xl
                     shadow-md hover:shadow-lg transition-shadow duration-[0.3s] ease
-                    w-full md:w-fit md:max-w-full md:min-w-[50%] md:max-h-[96%] flex flex-col my-4
+                    w-full md:w-fit md:max-w-full md:min-w-[50%] md:h-0 md:flex-grow
+                    flex flex-col my-4 overflow-hidden
                     ${className}`}
             >
-                <h2 id={`${id}-tile-title`} className="m-0 md:pb-2 md:border-b-2 border-solid text-xl md:text-3xl text-center font-semibold md:min-h-fit flex-none">{title}</h2>
-                <div className="flex flex-col gap-4 p-2.5 md:flex-grow md:overflow-y-auto">{children}</div>
+                <h2 id={`${id}-tile-title`} className="m-0 md:pb-2 md:border-b-2 border-solid text-xl md:text-3xl text-center font-semibold flex-none">{title}</h2>
+                <div className="flex flex-col gap-4 p-2.5 flex-grow overflow-y-auto">{children}</div>
             </div>
         </div>
     );
