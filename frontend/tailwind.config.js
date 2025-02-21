252c1a39fbd5aab4568fde7ac324dd5ee993f307
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-02-21 16:27:52 +0100

    Change default md screen size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,12 @@ export default {
     ],
     theme: {
         extend: {},
+        screens: {
+            sm: '640px',
+            md: '1024px',
+            lg: '1280px',
+            xl: '1536px',
+        },
     },
     plugins: [
         flowbite.plugin(),
