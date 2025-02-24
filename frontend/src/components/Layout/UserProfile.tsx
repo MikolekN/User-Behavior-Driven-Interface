d00d19f7182b2b279aacca925ea4e14842278443
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-02-24 15:59:54 +0100

    Better placement of language & translation

--- frontend/src/components/Layout/UserProfile.tsx
+++ frontend/src/components/Layout/UserProfile.tsx
@@ -15,6 +15,7 @@ import { UserIconContext } from '../../context/UserIconContext';
 import { AuthContext } from "../../context/AuthContext";
 import { useTranslation } from "react-i18next";
 import { LANGUAGES } from "../../pages/constants";
+import { content } from "flowbite-react/tailwind";
 
 export const UserProfile = () => {
     const navigate = useNavigate();
@@ -83,7 +84,7 @@ export const UserProfile = () => {
 
     const LanguageDropdownItem:React.FC<{image: string, name: string, code: string, isChosen: boolean}> = ({image, name, code, isChosen}) => {
         return (
-            <Dropdown.Item onClick={() => {handleChangeLanguage(code)}} className="space-x-2">
+            <Dropdown.Item onClick={() => {handleChangeLanguage(code)}} className="space-x-2 w-full">
                 <img src={image} alt="" className="w-5 h-5" />
                 <p>{name}</p>
                 <Checkbox defaultChecked disabled className={`w-4 h-4 ${isChosen ? '' : 'hidden'}`}/>
@@ -93,16 +94,20 @@ export const UserProfile = () => {
 
     const NestedDropdownItem: React.FC = () => {
         return (
-            <Dropdown arrowIcon={false} inline placement="right" theme={flowbiteDropdown}
-                label={
+            <Dropdown arrowIcon={false} inline placement="right"
+                renderTrigger={() => (
                     <button
                         type="button"
-                        className="w-full cursor-pointer items-center justify-start px-3 py-2 text-sm hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white flex bg-transparent text-black font-normal hover:font-semibold hover:text-black"
+                        className="w-full cursor-pointer items-center justify-start px-4 py-2 text-sm hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white flex bg-transparent text-black font-normal hover:font-semibold hover:text-black"
                     >
-                        <img src={LANGUAGES.find((language) => language.value === i18n.language)?.image} alt="" className="w-5 h-5 mr-1" />
-                        <p>{t('menu.languages.title')}</p>
+                        <img
+                            src={LANGUAGES.find((language) => language.value === i18n.language)?.image}
+                            alt=""
+                            className="w-5 h-5 mr-1"
+                        />
+                        <p>{t('menu.language')}</p>
                     </button>
-                }
+                )}
             >
                 {
                     LANGUAGES.map((language) => (
