32a20782e70bf865ef097cfb400fdd96b1c05da9
dlydka <lydkadm@gmail.com>
2025-03-14 17:28:38 +0100

    added Quick Icons mock mechanism

---
+++ frontend/src/components/Event/QuickIcons/QuickIcons.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { QUICK_ICONS } from "../../../event/utils/constants";
+import { Link } from "react-router-dom";
+import { DarkThemeToggle, Dropdown } from "flowbite-react";
+import { LANGUAGES } from "../../../pages/constants";
+import useHandleLogout from "../../../hooks/useHandleLogout";
+import LanguageDropdownItem from "../../LanguageDropdownItem/LanguageDropdownItem";
+import { t } from "i18next";
+import { flowbiteDropdownTheme } from "../../utils/themes/dropdownTheme";
+import { darkThemeToggleTheme } from "../../utils/themes/darkThemeToggleTheme";
+import i18n from "../../../i18n";
+
+
+const QuickIcons = () => {
+    const { handleLogout } = useHandleLogout();
+    const [quickIconsPreference, setQuickIconsPreference] = useState<string>("");
+
+    const getMockQuickIconsPreference = () => {
+        const ids = [QUICK_ICONS.PROFILE.id, QUICK_ICONS.SETTINGS.id, QUICK_ICONS.THEME_TOGGLE.id, QUICK_ICONS.LANGUAGE_SELECTOR.id, QUICK_ICONS.LOGOUT.id]
+        const randomElemId = Math.floor(Math.random()*ids.length);
+        return ids.at(randomElemId);
+    };
+
+    useEffect(() => {
+        setQuickIconsPreference(getMockQuickIconsPreference());
+    }, []);
+
+    const quickIconsElements = {
+        [QUICK_ICONS.PROFILE.id]: 
+            (<Link id={QUICK_ICONS.PROFILE.id} to="/profile" className="mx-1">
+                <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
+                    <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
+                </svg>
+            </Link>),
+        [QUICK_ICONS.SETTINGS.id]: 
+            (<Link id={QUICK_ICONS.SETTINGS.id} to="/settings" className="mx-1">
+                <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
+                </svg>
+            </Link>),
+        [QUICK_ICONS.THEME_TOGGLE.id]: 
+            (<DarkThemeToggle id={QUICK_ICONS.THEME_TOGGLE.id} theme={darkThemeToggleTheme}/>),
+        [QUICK_ICONS.LANGUAGE_SELECTOR.id]: 
+            (<Dropdown arrowIcon={false} inline placement="bottom" theme={flowbiteDropdownTheme}
+                label={
+                    <img id={QUICK_ICONS.LANGUAGE_SELECTOR.id} src={LANGUAGES.find((language) => language.value == i18n.language)?.image} alt="" className="w-5 h-5" />
+                }
+                >
+                {
+                    LANGUAGES.map((language) => {
+                        return(<LanguageDropdownItem image={language.image} name={t('menu.languages.' + language.key)} code={language.value} isChosen={i18n.language == language.value} />);
+                    })
+                }
+            </Dropdown>),
+        [QUICK_ICONS.LOGOUT.id]: 
+            (<Link id={QUICK_ICONS.LOGOUT.id} to="/" onClick={handleLogout} className="mx-1">
+                <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z" clip-rule="evenodd"/>
+                </svg>
+            </Link>)
+    };
+
+    return (
+        <div className="flex justify-center items-center">
+            {quickIconsPreference && quickIconsElements[quickIconsPreference]}
+        </div>
+    );
+};
+
+export default QuickIcons;
--- frontend/src/components/Layout/UserProfile.tsx
+++ frontend/src/components/Layout/UserProfile.tsx
@@ -1,38 +1,29 @@
 import { useContext, useEffect, useState } from "react";
-import { Dropdown, Avatar, Checkbox, DarkThemeToggle, useThemeMode, FlowbiteDarkThemeToggleTheme } from "flowbite-react"
-import { Link, useNavigate } from "react-router-dom";
-
+import { Dropdown, Avatar, DarkThemeToggle, useThemeMode } from "flowbite-react"
+import { Link } from "react-router-dom";
 import { UserContext } from '../../context/UserContext';
 import { UserIconContext } from '../../context/UserIconContext';
-import { AuthContext } from "../../context/AuthContext";
 import { useTranslation } from "react-i18next";
 import { LANGUAGES } from "../../pages/constants";
 import { setupUserDropdownClickEvents } from "../../event/utils/clickEvents";
 import { QUICK_ICONS, USER_DROPDOWN } from "../../event/utils/constants";
+import { flowbiteDropdownTheme } from "../utils/themes/dropdownTheme";
+import { darkThemeToggleTheme } from "../utils/themes/darkThemeToggleTheme";
+import DropdownItem from "../DropdownItem/DropdownItem";
+import LanguageDropdownItem from "../LanguageDropdownItem/LanguageDropdownItem";
+import NestedDropdownItem from "../NestedDropdownItem/NestedDropdownItem";
+import useHandleLogout from "../../hooks/useHandleLogout";
+import QuickIcons from "../Event/QuickIcons/QuickIcons";
 
 
 export const UserProfile = () => {
-    const navigate = useNavigate();
     const { i18n, t } = useTranslation();
     const { user } = useContext(UserContext);
-    const { logout } = useContext(AuthContext);
     const { getIcon } = useContext(UserIconContext);
     const { computedMode, toggleMode } = useThemeMode();
+    const { handleLogout } = useHandleLogout();
 
     const [iconSrc, setIconSrc] = useState<string | null>(null);
-
-    const handleChangeLanguage = (lang_code: string) => {
-        i18n.changeLanguage(lang_code);
-        localStorage.setItem('language', lang_code);
-    }
-
-    const handleLogout = async () => {
-        try {
-            await logout();
-        } catch { /* If error occurs it is displayed in console */ } finally {
-            navigate('/');
-        }
-    };
 
     useEffect(() => {
         const fetchIcon = async () => {
@@ -56,75 +47,6 @@ export const UserProfile = () => {
         void fetchIcon();
     }, [user, getIcon, user?.icon, computedMode]);
 
-    const DropdownItem:React.FC<{id: string, label: string, path: string, icon: string | React.ReactNode, className?: string, onClick?: () => Promise<void>}> = ({id, label, path, icon, className, onClick}) => {
-        return (
-            <Dropdown.Item id={id} as={Link} to={path} onClick={onClick} className="flex bg-transparent text-black font-normal hover:font-semibold hover:text-black">
-                {typeof icon === 'string' ? (
-                    <img src={icon} className={` ${className} w-5 h-5 mr-1`} />
-                ) : (
-                    <span className={` ${className} w-5 h-5 mr-1 flex items-center justify-center`}>{icon}</span>
-                )}
-                {label}
-            </Dropdown.Item>
-        )
-    }
-
-    const LanguageDropdownItem:React.FC<{image: string, name: string, code: string, isChosen: boolean}> = ({image, name, code, isChosen}) => {
-        return (
-            <Dropdown.Item onClick={() => {handleChangeLanguage(code)}} className="space-x-2 w-full">
-                <img src={image} alt="" className="w-5 h-5" />
-                <p>{name}</p>
-                <Checkbox defaultChecked disabled className={`w-4 h-4 ${isChosen ? '' : 'hidden'}`}/>
-            </Dropdown.Item>
-        )
-    }
-
-    const NestedDropdownItem: React.FC<{id: string}> = ({id}) => {
-        return (
-            
-                <Dropdown arrowIcon={false} inline placement="right"
-                    renderTrigger={() => (
-                        <button
-                            id={id}
-                            type="button"
-                            className="w-full cursor-pointer items-center justify-start px-4 py-2 text-sm hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white flex bg-transparent text-black font-normal hover:font-semibold hover:text-black"
-                        >
-                            <img
-                                src={LANGUAGES.find((language) => language.value === i18n.language)?.image}
-                                alt=""
-                                className="w-5 h-5 mr-1"
-                            />
-                            <p>{t('menu.language')}</p>
-                        </button>
-                    )}
-                >
-                    {
-                        LANGUAGES.map((language) => (
-                            <LanguageDropdownItem
-                                key={language.value}
-                                image={language.image}
-                                name={t('menu.languages.' + language.key)}
-                                code={language.value}
-                                isChosen={i18n.language === language.value}
-                            />
-                        ))
-                    }
-                </Dropdown>
-           
-        );
-    };
-
-    const darkThemeToggle: FlowbiteDarkThemeToggleTheme = {
-        root: {
-            base: "block mx-1",
-            icon: "w-5 h-5"
-        }
-    };
-
-    const flowbiteDropdown = {
-        inlineWrapper: "mx-1"
-    };
-
     useEffect(() => {
         const clickEvents = setupUserDropdownClickEvents(user);
         return clickEvents;
@@ -135,9 +57,8 @@ export const UserProfile = () => {
             {!user && (
                 <>
                     <div className="flex justify-center items-center">
-                        <DarkThemeToggle id={QUICK_ICONS.THEME_TOGGLE.id} theme={darkThemeToggle}/>
-
-                        <Dropdown arrowIcon={false} inline placement="bottom" theme={flowbiteDropdown}
+                        <DarkThemeToggle id={QUICK_ICONS.THEME_TOGGLE.id} theme={darkThemeToggleTheme}/>
+                        <Dropdown arrowIcon={false} inline placement="bottom" theme={flowbiteDropdownTheme}
                             label={
                                 <img id={QUICK_ICONS.LANGUAGE_SELECTOR.id} src={LANGUAGES.find((language) => language.value == i18n.language)?.image} alt="" className="w-5 h-5" />
                             }
@@ -148,7 +69,6 @@ export const UserProfile = () => {
                                 })
                             }
                         </Dropdown>
-
                         <Link id={QUICK_ICONS.LOGIN.id} to="/login" className="mx-1">
                             <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                 <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
@@ -169,7 +89,7 @@ export const UserProfile = () => {
                         }
                     >
                         <Dropdown.Item id={USER_DROPDOWN.THEME_TOGGLE.id} onClick={toggleMode} className='bg-transparent text-black font-normal hover:font-semibold hover:text-black px-3'>
-                            <DarkThemeToggle theme={darkThemeToggle}/>
+                            <DarkThemeToggle theme={darkThemeToggleTheme}/>
                             <p>{t('menu.profile.mode.' + computedMode)}</p>
                         </Dropdown.Item>
                         <NestedDropdownItem id={USER_DROPDOWN.LANGUAGE_SELECTOR.id} />
@@ -193,35 +113,7 @@ export const UserProfile = () => {
 
             {user && (
                 <>
-                    <div className="flex justify-center items-center">
-                        <Link id={QUICK_ICONS.PROFILE.id} to="/profile" className="mx-1">
-                            <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
-                                <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
-                            </svg>
-                        </Link>
-                        <Link id={QUICK_ICONS.SETTINGS.id} to="/settings" className="mx-1">
-                            <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
-                            </svg>
-                        </Link>
-                        <DarkThemeToggle id={QUICK_ICONS.THEME_TOGGLE.id} theme={darkThemeToggle}/>
-                        <Dropdown arrowIcon={false} inline placement="bottom" theme={flowbiteDropdown}
-                            label={
-                                <img id={QUICK_ICONS.LANGUAGE_SELECTOR.id} src={LANGUAGES.find((language) => language.value == i18n.language)?.image} alt="" className="w-5 h-5" />
-                            }
-                        >
-                            {
-                                LANGUAGES.map((language) => {
-                                    return(<LanguageDropdownItem image={language.image} name={t('menu.languages.' + language.key)} code={language.value} isChosen={i18n.language == language.value} />);
-                                })
-                            }
-                        </Dropdown>
-                        <Link id={QUICK_ICONS.LOGOUT.id} to="/" onClick={handleLogout} className="mx-1">
-                            <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z" clip-rule="evenodd"/>
-                            </svg>
-                        </Link>
-                    </div>
+                    <QuickIcons />
                     <Dropdown arrowIcon={false} inline placement="bottom"
                         label={
                             <>
@@ -255,7 +147,7 @@ export const UserProfile = () => {
                             }
                         />
                         <Dropdown.Item id={USER_DROPDOWN.THEME_TOGGLE.id} onClick={toggleMode} className='bg-transparent text-black font-normal hover:font-semibold hover:text-black px-3'>
-                            <DarkThemeToggle theme={darkThemeToggle}/>
+                            <DarkThemeToggle theme={darkThemeToggleTheme}/>
                             <p>{t('menu.profile.mode.' + computedMode)}</p>
                         </Dropdown.Item>
                         <NestedDropdownItem id={USER_DROPDOWN.LANGUAGE_SELECTOR.id} />
@@ -270,7 +162,6 @@ export const UserProfile = () => {
                     </Dropdown>
                 </>
             )}
-
         </div>
     );
-}
+};
