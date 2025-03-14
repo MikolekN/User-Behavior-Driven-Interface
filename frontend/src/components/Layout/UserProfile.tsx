51dd3460d46c84823da3c4cb5b15e91b01904b38
dlydka <lydkadm@gmail.com>
2025-03-14 11:49:38 +0100

    added sending click events Quick Icons

--- frontend/src/components/Layout/UserProfile.tsx
+++ frontend/src/components/Layout/UserProfile.tsx
@@ -7,6 +7,9 @@ import { UserIconContext } from '../../context/UserIconContext';
 import { AuthContext } from "../../context/AuthContext";
 import { useTranslation } from "react-i18next";
 import { LANGUAGES } from "../../pages/constants";
+import { setupUserDropdownClickEvents } from "../../event/utils/clickEvents";
+import { QUICK_ICONS, USER_DROPDOWN } from "../../event/utils/constants";
+
 
 export const UserProfile = () => {
     const navigate = useNavigate();
@@ -53,9 +56,9 @@ export const UserProfile = () => {
         void fetchIcon();
     }, [user, getIcon, user?.icon, computedMode]);
 
-    const DropdownItem:React.FC<{label: string, path: string, icon: string | React.ReactNode, className?: string, onClick?: () => Promise<void>}> = ({label, path, icon, className, onClick}) => {
+    const DropdownItem:React.FC<{id: string, label: string, path: string, icon: string | React.ReactNode, className?: string, onClick?: () => Promise<void>}> = ({id, label, path, icon, className, onClick}) => {
         return (
-            <Dropdown.Item as={Link} to={path} onClick={onClick} className="flex bg-transparent text-black font-normal hover:font-semibold hover:text-black">
+            <Dropdown.Item id={id} as={Link} to={path} onClick={onClick} className="flex bg-transparent text-black font-normal hover:font-semibold hover:text-black">
                 {typeof icon === 'string' ? (
                     <img src={icon} className={` ${className} w-5 h-5 mr-1`} />
                 ) : (
@@ -76,35 +79,38 @@ export const UserProfile = () => {
         )
     }
 
-    const NestedDropdownItem: React.FC = () => {
+    const NestedDropdownItem: React.FC<{id: string}> = ({id}) => {
         return (
-            <Dropdown arrowIcon={false} inline placement="right"
-                renderTrigger={() => (
-                    <button
-                        type="button"
-                        className="w-full cursor-pointer items-center justify-start px-4 py-2 text-sm hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white flex bg-transparent text-black font-normal hover:font-semibold hover:text-black"
-                    >
-                        <img
-                            src={LANGUAGES.find((language) => language.value === i18n.language)?.image}
-                            alt=""
-                            className="w-5 h-5 mr-1"
-                        />
-                        <p>{t('menu.language')}</p>
-                    </button>
-                )}
-            >
-                {
-                    LANGUAGES.map((language) => (
-                        <LanguageDropdownItem
-                            key={language.value}
-                            image={language.image}
-                            name={t('menu.languages.' + language.key)}
-                            code={language.value}
-                            isChosen={i18n.language === language.value}
-                        />
-                    ))
-                }
-            </Dropdown>
+            
+                <Dropdown arrowIcon={false} inline placement="right"
+                    renderTrigger={() => (
+                        <button
+                            id={id}
+                            type="button"
+                            className="w-full cursor-pointer items-center justify-start px-4 py-2 text-sm hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white flex bg-transparent text-black font-normal hover:font-semibold hover:text-black"
+                        >
+                            <img
+                                src={LANGUAGES.find((language) => language.value === i18n.language)?.image}
+                                alt=""
+                                className="w-5 h-5 mr-1"
+                            />
+                            <p>{t('menu.language')}</p>
+                        </button>
+                    )}
+                >
+                    {
+                        LANGUAGES.map((language) => (
+                            <LanguageDropdownItem
+                                key={language.value}
+                                image={language.image}
+                                name={t('menu.languages.' + language.key)}
+                                code={language.value}
+                                isChosen={i18n.language === language.value}
+                            />
+                        ))
+                    }
+                </Dropdown>
+           
         );
     };
 
@@ -117,18 +123,23 @@ export const UserProfile = () => {
 
     const flowbiteDropdown = {
         inlineWrapper: "mx-1"
-      };
+    };
+
+    useEffect(() => {
+        const clickEvents = setupUserDropdownClickEvents(user);
+        return clickEvents;
+    }, [user]);
 
     return (
         <div className="flex order-3 md:order-3 space-x-2">
             {!user && (
                 <>
                     <div className="flex justify-center items-center">
-                        <DarkThemeToggle theme={darkThemeToggle}/>
+                        <DarkThemeToggle id={QUICK_ICONS.THEME_TOGGLE.id} theme={darkThemeToggle}/>
 
                         <Dropdown arrowIcon={false} inline placement="bottom" theme={flowbiteDropdown}
                             label={
-                                <img src={LANGUAGES.find((language) => language.value == i18n.language)?.image} alt="" className="w-5 h-5" />
+                                <img id={QUICK_ICONS.LANGUAGE_SELECTOR.id} src={LANGUAGES.find((language) => language.value == i18n.language)?.image} alt="" className="w-5 h-5" />
                             }
                         >
                             {
@@ -138,13 +149,13 @@ export const UserProfile = () => {
                             }
                         </Dropdown>
 
-                        <Link to="/login" className="mx-1">
+                        <Link id={QUICK_ICONS.LOGIN.id} to="/login" className="mx-1">
                             <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                 <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
                             </svg>
                         </Link>
 
-                        <Link to="/register" className="mx-1">
+                        <Link id={QUICK_ICONS.REGISTER.id} to="/register" className="mx-1">
                             <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                 <path fillRule="evenodd" d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z" clipRule="evenodd"/>
                             </svg>
@@ -152,24 +163,24 @@ export const UserProfile = () => {
                     </div>
                     <Dropdown arrowIcon={false} inline placement="bottom"
                         label={
-                            <svg className="w-10 h-10 text-gray-800 dark:text-gray-400 rounded-full hover:bg-gray-200 hover:dark:bg-gray-700 transition ease-in-out duration-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
+                            <svg  className="w-10 h-10 text-gray-800 dark:text-gray-400 rounded-full hover:bg-gray-200 hover:dark:bg-gray-700 transition ease-in-out duration-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                 <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
                             </svg>
                         }
                     >
-                        <Dropdown.Item onClick={toggleMode} className='bg-transparent text-black font-normal hover:font-semibold hover:text-black px-3'>
+                        <Dropdown.Item id={USER_DROPDOWN.THEME_TOGGLE.id} onClick={toggleMode} className='bg-transparent text-black font-normal hover:font-semibold hover:text-black px-3'>
                             <DarkThemeToggle theme={darkThemeToggle}/>
                             <p>{t('menu.profile.mode.' + computedMode)}</p>
                         </Dropdown.Item>
-                        <NestedDropdownItem />
-                        <DropdownItem label={t('menu.profile.login')} path="/login"
+                        <NestedDropdownItem id={USER_DROPDOWN.LANGUAGE_SELECTOR.id} />
+                        <DropdownItem id={USER_DROPDOWN.LOGIN.id} label={t('menu.profile.login')} path="/login"
                             icon={
                                 <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                     <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
                                 </svg>
                             }
                         />
-                        <DropdownItem label={t('menu.profile.register')} path="/register" className="ml-1"
+                        <DropdownItem id={USER_DROPDOWN.REGISTER.id} label={t('menu.profile.register')} path="/register" className="ml-1"
                             icon={
                                 <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                     <path fillRule="evenodd" d="M9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H7Zm8-1a1 1 0 0 1 1-1h1v-1a1 1 0 1 1 2 0v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0v-1h-1a1 1 0 0 1-1-1Z" clipRule="evenodd"/>
@@ -183,23 +194,20 @@ export const UserProfile = () => {
             {user && (
                 <>
                     <div className="flex justify-center items-center">
-                        <Link to="/profile" className="mx-1">
+                        <Link id={QUICK_ICONS.PROFILE.id} to="/profile" className="mx-1">
                             <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                 <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
                             </svg>
                         </Link>
-
-                        <Link to="/settings" className="mx-1">
+                        <Link id={QUICK_ICONS.SETTINGS.id} to="/settings" className="mx-1">
                             <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
                             </svg>
                         </Link>
-
-                        <DarkThemeToggle theme={darkThemeToggle}/>
-
+                        <DarkThemeToggle id={QUICK_ICONS.THEME_TOGGLE.id} theme={darkThemeToggle}/>
                         <Dropdown arrowIcon={false} inline placement="bottom" theme={flowbiteDropdown}
                             label={
-                                <img src={LANGUAGES.find((language) => language.value == i18n.language)?.image} alt="" className="w-5 h-5" />
+                                <img id={QUICK_ICONS.LANGUAGE_SELECTOR.id} src={LANGUAGES.find((language) => language.value == i18n.language)?.image} alt="" className="w-5 h-5" />
                             }
                         >
                             {
@@ -208,14 +216,12 @@ export const UserProfile = () => {
                                 })
                             }
                         </Dropdown>
-
-                        <Link to="/" onClick={handleLogout} className="mx-1">
+                        <Link id={QUICK_ICONS.LOGOUT.id} to="/" onClick={handleLogout} className="mx-1">
                             <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z" clip-rule="evenodd"/>
                             </svg>
                         </Link>
                     </div>
-
                     <Dropdown arrowIcon={false} inline placement="bottom"
                         label={
                             <>
@@ -234,27 +240,27 @@ export const UserProfile = () => {
                             <span className="block text-sm">{user?.login}</span>
                             <span className="block truncate text-sm font-medium">{user?.email}</span>
                         </Dropdown.Header>
-                        <DropdownItem label={t('menu.profile.profile')} path="/profile"
+                        <DropdownItem id={USER_DROPDOWN.PROFILE.id} label={t('menu.profile.profile')} path="/profile"
                             icon={
                                 <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="2 2 20 20">
                                     <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
                                 </svg>
                             }
                         />
-                        <DropdownItem label={t('menu.profile.settings')} path="/settings"
+                        <DropdownItem id={USER_DROPDOWN.SETTINGS.id} label={t('menu.profile.settings')} path="/settings"
                             icon={
                                 <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path fill-rule="evenodd" d="M9.586 2.586A2 2 0 0 1 11 2h2a2 2 0 0 1 2 2v.089l.473.196.063-.063a2.002 2.002 0 0 1 2.828 0l1.414 1.414a2 2 0 0 1 0 2.827l-.063.064.196.473H20a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-.089l-.196.473.063.063a2.002 2.002 0 0 1 0 2.828l-1.414 1.414a2 2 0 0 1-2.828 0l-.063-.063-.473.196V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.089l-.473-.196-.063.063a2.002 2.002 0 0 1-2.828 0l-1.414-1.414a2 2 0 0 1 0-2.827l.063-.064L4.089 15H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h.09l.195-.473-.063-.063a2 2 0 0 1 0-2.828l1.414-1.414a2 2 0 0 1 2.827 0l.064.063L9 4.089V4a2 2 0 0 1 .586-1.414ZM8 12a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z" clip-rule="evenodd"/>
                                 </svg>
                             }
                         />
-                        <Dropdown.Item onClick={toggleMode} className='bg-transparent text-black font-normal hover:font-semibold hover:text-black px-3'>
+                        <Dropdown.Item id={USER_DROPDOWN.THEME_TOGGLE.id} onClick={toggleMode} className='bg-transparent text-black font-normal hover:font-semibold hover:text-black px-3'>
                             <DarkThemeToggle theme={darkThemeToggle}/>
                             <p>{t('menu.profile.mode.' + computedMode)}</p>
                         </Dropdown.Item>
-                        <NestedDropdownItem />
+                        <NestedDropdownItem id={USER_DROPDOWN.LANGUAGE_SELECTOR.id} />
                         <Dropdown.Divider />
-                        <DropdownItem label={t('menu.profile.logout')} path="/" onClick={handleLogout}
+                        <DropdownItem id={USER_DROPDOWN.LOGOUT.id} label={t('menu.profile.logout')} path="/" onClick={handleLogout}
                             icon={
                                 <svg className="w-5 h-5 text-gray-800 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z" clip-rule="evenodd"/>
