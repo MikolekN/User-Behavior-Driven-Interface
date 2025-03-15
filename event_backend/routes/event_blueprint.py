efc60feab6b51548bdb8f799f50a2686df241b4c
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-03-15 10:59:56 +0100

    Use headers for token example

--- event_backend/routes/event_blueprint.py
+++ event_backend/routes/event_blueprint.py
@@ -7,7 +7,7 @@ from routes.get_preferences import get_preferences
 
 event_blueprint = Blueprint('event', __name__, url_prefix='/api')
 
-event_blueprint.add_url_rule('/events/get', 'get_events', get_events, methods=['POST'])
+event_blueprint.add_url_rule('/events/<user_id>', 'get_events', get_events, methods=['Get'])
 event_blueprint.add_url_rule('/events/create', 'create_event', create_event, methods=['POST'])
 event_blueprint.add_url_rule('/preferences/get', 'get_preferences', get_preferences, methods=['POST'])
 event_blueprint.add_url_rule('/preferences/generate', 'generate_preferences', generate_preferences, methods=['POST'])
--- event_backend/routes/get_events.py
+++ event_backend/routes/get_events.py
@@ -5,7 +5,6 @@ from flask import Response, request
 from shared import create_simple_response
 
 from click_events.click_event_repository import ClickEventRepository
-from click_events.requests.get_click_events_request import GetClickEventsRequest
 from click_events.responses.get_click_events_response import GetClickEventsResponse
 from shared import Token
 from shared import TokenRepository
@@ -13,26 +12,29 @@ from shared import TokenRepository
 click_event_repository = ClickEventRepository()
 token_repository = TokenRepository()
 
-def get_events() -> Response:
-    data = request.get_json()
+def get_events(user_id) -> Response:
+    if not isinstance(user_id, str) or not bson.ObjectId.is_valid(user_id):
+        return create_simple_response("invalidUser", HTTPStatus.BAD_REQUEST)
+    user_obj_id = bson.ObjectId(user_id)
 
-    error = GetClickEventsRequest.validate_request(data)
-    if error:
-        return create_simple_response(error, HTTPStatus.BAD_REQUEST)
+    token_value = request.headers.get("Authorization")
+    if not token_value:
+        return create_simple_response("missingToken", HTTPStatus.UNAUTHORIZED)
 
-    token: Token = token_repository.find_by_user(data.get("user_id"))
+    token: Token = token_repository.find_by_user(user_obj_id)
     if not token:
         return create_simple_response("token not exist", HTTPStatus.NOT_FOUND)
 
-    if token.token != data.get("token"):
+    if token.token != token_value:
         return create_simple_response("wrong token", HTTPStatus.BAD_REQUEST)
 
     query = {
-        "user_id": bson.ObjectId(data.get("user_id")),
+        "user_id": user_obj_id,
         "event_type": "click_event"
     }
     sort_criteria = [("start_timestamp", 1)]
     events = click_event_repository.find_many(query, sort_criteria)
+
     if not events:
         return create_simple_response("noEvents", HTTPStatus.NOT_FOUND)
     events_dto = [event.to_dict() for event in events]
