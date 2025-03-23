103fcbd247543b90e51d1b673849c6599fdfea77
dlydka <lydkadm@gmail.com>
2025-03-23 15:59:57 +0100

    added block of previous opened tab
    previous active (opened) tab is blocked, nothing is displayed and alert in both languages is displayed

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Layout } from '../components/Layout/Layout';
 import { Suspense, useContext, useEffect } from 'react';
+import { Suspense, useContext, useEffect, useState } from 'react';
 import DefaultLoadingSkeleton from '../components/Loading/DefaultLoadingSkeleton';
 import { setupSubmitButtonsClickEvents, setupUserDropdownClickEvents } from '../event/utils/clickEvents';
 import { UserContext } from '../context/UserContext';
+import { t } from 'i18next';
 
 const App = () => {
     const { user } = useContext(UserContext);
-    
+    const [isTabOpen, setIsTabOpen] = useState<boolean>(false);
+
     useEffect(() => {
         const userDropdownClickEvents = setupUserDropdownClickEvents(user);
         const submitButtonsClickEvents = setupSubmitButtonsClickEvents(user);
@@ -17,6 +20,32 @@ const App = () => {
             submitButtonsClickEvents();
         };
     }, [user]);
+
+    useEffect(() => {
+        const uniqueTabId = Date.now().toString();
+        localStorage.setItem("tabId", uniqueTabId);
+    
+        const handleStorageChange = (event: StorageEvent) => {
+          if (event.key === "tabId") {
+				alert(t('blockedTabInfo'));
+				setIsTabOpen(true);
+          }
+        };
+    
+        window.addEventListener("storage", handleStorageChange);
+    
+        return () => {
+			window.removeEventListener("storage", handleStorageChange);
+			localStorage.removeItem("tabId"); // Clean up when tab is closed
+			setIsTabOpen(false);
+        };
+    }, []);
+
+	if (isTabOpen) {
+		return (
+			<></>
+		);
+	};
 
     return (
         <Suspense fallback={<DefaultLoadingSkeleton />}>
