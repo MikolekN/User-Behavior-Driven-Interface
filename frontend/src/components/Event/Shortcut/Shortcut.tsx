708fe9ebaa8b72ece61a50bfacd844a849bba8c9
dlydka <lydkadm@gmail.com>
2025-03-26 17:47:46 +0100

    prepared Shortcut component with translations of possible links

--- frontend/src/components/Event/Shortcut/Shortcut.tsx
+++ frontend/src/components/Event/Shortcut/Shortcut.tsx
@@ -1,44 +1,54 @@
 import { Link } from "react-router-dom";
-import Button from "../../utils/Button";
 import { t } from "i18next";
 import Tile from "../../Tile/Tile";
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../../../context/UserContext";
+import useApiErrorHandler from "../../../hooks/useApiErrorHandler";
+import { PreferencesContext } from "../../../event/context/PreferencesContext";
+import externalLink  from "../../../assets/images/external-link.svg";
 
 const Shortcut = () => {
-    const [links, setLinks] = useState<string[]>([]);
+    const { handleError } = useApiErrorHandler();
+    const { user } = useContext(UserContext);
+    const { shortcutPreference, getShortcutPreference } = useContext(PreferencesContext);
 
     useEffect(() => {
-        const setMockShortcutLinks = () => {
-            setLinks(["cyclic-payments", "transfer", "loan", "dashboard"]);
-        }
-        
-        setMockShortcutLinks();
-    }, []);
+        if (!user) return;
+        const fetchQuickIconsPreferences = async () => {
+            try {
+                getShortcutPreference();
+            } catch (error) {
+                handleError(error);
+            }
+        };
+
+        void fetchQuickIconsPreferences();
+    }, [user, getShortcutPreference]);
 
     return (
         <>
             <div className="hidden md:flex">
-                <Tile title="Shortcut">
-                    {links.map((link, index) => (
-                        <Link key={index} to={`/${link}`} className="m-2">
-                            
-                                {/* {t('pageNotFound.backToDashboard')} */}
-                                {link}
-                            
-                        </Link> 
+                <Tile title={t('shortcut.tile.title')}>
+                    {shortcutPreference?.links.map((link, index) => (
+                        <div className="flex">
+                            <Link key={index} to={`${link}`} className="m-2 hover:text-blue-700 hover:font-semibold">
+                                {t(`shortcut.${link}`)}
+                            </Link>
+                            <img src={externalLink} alt="externalLink" width={16} height={16} />
+                        </div>
                     ))}
                 </Tile>
             </div>
             <div className="md:hidden">
-                <Tile title="Shortcut">
-                    <div className="flex flex-row">
-                        {links.map((link, index) => (
-                            <Link key={index} to={`/${link}`} className="m-2">
-                                
-                                    {/* {t('pageNotFound.backToDashboard')} */}
-                                    {link}
-                                
-                            </Link> 
+                <Tile title={t('shortcut.tile.title')}>
+                    <div className="w-full flex justify-center flex-wrap">
+                        {shortcutPreference?.links.map((link, index) => (
+                           <div className="flex">
+                                <Link key={index} to={`${link}`} className="m-2 hover:text-blue-700 hover:font-semibold">
+                                    {t(`shortcut.${link}`)}
+                                </Link>
+                                <img src={externalLink} alt="externalLink" width={16} height={16} />
+                            </div>
                         ))}
                     </div>
                 </Tile>
