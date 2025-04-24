a140a2a6aa74adb81f2a876792de01705cfe67bf
Mikołaj Nadzieja <mikolaj.nadzieja@wp.pl>
2025-04-24 11:07:42 +0200

    Create hover event

---
+++ event_backend/routes/event/create_event/create_hover_event.py
@@ -0,0 +1,42 @@
+from http import HTTPStatus
+
+import bson
+from flask import Response, request
+from shared import create_simple_response
+
+from hover_events.hover_event import HoverEvent
+from hover_events.hover_event_repository import HoverEventRepository
+from hover_events.requests.create_hover_event_request import CreateHoverEventRequest
+from routes.helpers import validate_token
+
+hover_event_repository = HoverEventRepository()
+
+def create_hover_event(user_id: str, data: dict) -> Response:
+    error = CreateHoverEventRequest.validate_request(data)
+    if error:
+        return create_simple_response(error, HTTPStatus.BAD_REQUEST)
+
+    if not isinstance(user_id, str) or not bson.ObjectId.is_valid(user_id):
+        return create_simple_response("invalidUser", HTTPStatus.BAD_REQUEST)
+    user_obj_id = bson.ObjectId(user_id)
+
+    token_value = request.headers.get("Authorization")
+    invalid = validate_token(token_value, user_obj_id)
+    if invalid:
+        return invalid
+
+    hover_event = HoverEvent(
+        session_id=None,
+        case_id=None,
+        event_id=None,
+        user_id=user_obj_id,
+        start_timestamp=data.get('start_timestamp'),
+        event_type=data.get('event_type'),
+        page=data.get('page'),
+        element_id=data.get('element_id'),
+        end_timestamp=data.get('end_timestamp'),
+        duration=data.get('duration'),
+    )
+    hover_event_repository.insert(hover_event)
+
+    return create_simple_response("hoverEventCreatedSuccessfully", HTTPStatus.CREATED)
